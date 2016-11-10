c79f74a06db01dbc9f84b0a16438fd893229e88f
AR Soltani <alireza.soltani1373@gmail.com>
2016-11-11 01:40:38 +0330

    Silde Nav Added

--- ProposalReportingSystem/ProposalReportingSystem/Form1.Designer.cs
+++ ProposalReportingSystem/ProposalReportingSystem/Form1.Designer.cs
@@ -28,12 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.sideNav1 = new DevComponents.DotNetBar.Controls.SideNav();
+            this.sideNavItem1 = new DevComponents.DotNetBar.Controls.SideNavItem();
+            this.separator1 = new DevComponents.DotNetBar.Separator();
+            this.Home_tab = new DevComponents.DotNetBar.Controls.SideNavItem();
+            this.sideNavPanel1 = new DevComponents.DotNetBar.Controls.SideNavPanel();
+            this.sideNav1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // sideNav1
+            // 
+            this.sideNav1.Controls.Add(this.sideNavPanel1);
+            this.sideNav1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.sideNav1.Font = new System.Drawing.Font("Iranian Sans", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.sideNav1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.sideNav1.Items.AddRange(new DevComponents.DotNetBar.BaseItem[] {
+            this.sideNavItem1,
+            this.separator1,
+            this.Home_tab});
+            this.sideNav1.Location = new System.Drawing.Point(0, 0);
+            this.sideNav1.Name = "sideNav1";
+            this.sideNav1.Padding = new System.Windows.Forms.Padding(1);
+            this.sideNav1.Size = new System.Drawing.Size(1045, 673);
+            this.sideNav1.TabIndex = 0;
+            this.sideNav1.Text = "sideNav1";
+            // 
+            // sideNavItem1
+            // 
+            this.sideNavItem1.IsSystemMenu = true;
+            this.sideNavItem1.Name = "sideNavItem1";
+            this.sideNavItem1.Symbol = "";
+            this.sideNavItem1.Text = "Menu";
+            // 
+            // separator1
+            // 
+            this.separator1.FixedSize = new System.Drawing.Size(3, 1);
+            this.separator1.Name = "separator1";
+            this.separator1.Padding.Bottom = 2;
+            this.separator1.Padding.Left = 6;
+            this.separator1.Padding.Right = 6;
+            this.separator1.Padding.Top = 2;
+            this.separator1.SeparatorOrientation = DevComponents.DotNetBar.eDesignMarkerOrientation.Vertical;
+            // 
+            // Home_tab
+            // 
+            this.Home_tab.Checked = true;
+            this.Home_tab.Name = "Home_tab";
+            this.Home_tab.Panel = this.sideNavPanel1;
+            this.Home_tab.Symbol = "";
+            this.Home_tab.Text = "خانه";
+            // 
+            // sideNavPanel1
+            // 
+            this.sideNavPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.sideNavPanel1.Location = new System.Drawing.Point(87, 36);
+            this.sideNavPanel1.Name = "sideNavPanel1";
+            this.sideNavPanel1.Size = new System.Drawing.Size(953, 636);
+            this.sideNavPanel1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1043, 673);
+            this.Controls.Add(this.sideNav1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.sideNav1.ResumeLayout(false);
+            this.sideNav1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private DevComponents.DotNetBar.Controls.SideNav sideNav1;
+        private DevComponents.DotNetBar.Controls.SideNavPanel sideNavPanel1;
+        private DevComponents.DotNetBar.Controls.SideNavItem sideNavItem1;
+        private DevComponents.DotNetBar.Separator separator1;
+        private DevComponents.DotNetBar.Controls.SideNavItem Home_tab;
     }
 }
 
