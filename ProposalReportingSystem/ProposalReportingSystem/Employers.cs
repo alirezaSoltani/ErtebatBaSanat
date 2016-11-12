e9c12f26b154b7d62aacbc37029e7841f603c965
AR Soltani <alireza.soltani1373@gmail.com>
2016-11-12 12:50:55 +0330

    Empiyers added

---
+++ ProposalReportingSystem/ProposalReportingSystem/Employers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProposalReportingSystem
+{
+   
+    class Employers
+    {
+        
+        private string orgName;
+        public Employers(string orgName) {
+            this.orgName = orgName;
+        }
+        public string getOrgName() {
+            return orgName;
+        }
+        public void setOrgName(String orgName) {
+            this.orgName = orgName;
+        }
+    }
+}
